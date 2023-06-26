3d2d95b2c232d07dd08edac2a974521fd6237304
NatnaelTeshome <natnaelmk@gmail.com>
2023-06-26 16:18:42 -0400

    Automatic upload folder file deletion upon reloading the home page

--- flask/hello.py
+++ flask/hello.py
@@ -1,9 +1,10 @@
 import csv
+import os
 from flask import Flask, flash, request, redirect, url_for, send_from_directory, render_template
 from werkzeug.utils import secure_filename
+import datetime
 
 from scrape_main import search
-
 UPLOAD_FOLDER = './uploads'
 ALLOWED_EXTENSIONS = {'csv'}
 
@@ -17,6 +18,10 @@ def allowed_file(filename):
 # Home page
 @app.route('/')
 def index():
+    # Get all the files in the uploads folder
+    files = os.listdir(app.config['UPLOAD_FOLDER'])
+    for file in files:
+        os.remove(os.path.join(app.config['UPLOAD_FOLDER'], file))
     return render_template('index.html')
 
 # Create a file
@@ -30,8 +35,13 @@ def create():
         if not search_term or not file_name:
             return redirect(request.url)
         file_name = secure_filename(file_name)
+        current_datetime = datetime.datetime.now()
+        formatted_datetime = current_datetime.strftime("%Y_%m_%d_%H_%M")
+        file_name = "{}_{}".format(file_name, formatted_datetime)
         file_name = "{}.csv".format(file_name)
-        full_file_name = "./uploads/{}".format(file_name)
+
+    
+        full_file_name = "{}/{}".format(app.config["UPLOAD_FOLDER"], file_name)
         column_names = ["URL", "Date", "Title", "Author", "Type", "Price", "Summary", "Publication Year", "Language", "ISBN", "Category", "Copyright", "Contributors",
         "Pages", "Binding", "Interior Color", "Dimensions", "Format", "Keywords"]
         with open(full_file_name, 'w', encoding="utf-8", newline='') as f:
@@ -55,8 +65,12 @@ def upload():
         if not file.filename or not allowed_file(file.filename):
             return redirect(request.url)
         filename = secure_filename(file.filename)
+        current_datetime = datetime.datetime.now()
+        formatted_datetime = current_datetime.strftime("%Y_%m_%d_%H_%M_%S")
+        filename = "{}_{}".format(filename, formatted_datetime)
+        filename = "{}.csv".format(filename)
         search_term = request.form['search_term']
-        full_file_name = "./uploads/" + filename
+        full_file_name = app.config["UPLOAD_FOLDER"] + filename
         file.save(full_file_name)
         search(search_term, full_file_name)
         return redirect(url_for('download_file', name=filename))
